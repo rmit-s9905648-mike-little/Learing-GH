d9f469f6667e363a7c9f1b2d0abc244926211766
FlashJackFromGungagai <hypercomms2001@gmail.com>
2023-07-13 22:03:20 +1000

    I added "my model.swift" , and added a new struct "mymodel" in this file

---
+++ Learning-GH/mymodel.swift
@@ -0,0 +1,16 @@
+//
+//  mymodel.swift
+//  Learning-GH
+//
+//  Created by mlittle-macmini-20200921 on 13/7/2023.
+//
+
+import Foundation
+
+// let us add an arbitrary class, or struct
+// just so we can have some program Mick content in this new file.
+struct  mymodel
+{
+    var someName: String
+    
+}// Close Press that
